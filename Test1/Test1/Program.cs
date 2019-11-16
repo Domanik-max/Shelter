f19fa278d58b7662c1dede22177e44d06c11db45
Domanik Johnson <domaniklavonn@gmaol.com>
2019-11-15 21:04:36 -0600

    Deleted some files and added correct files

--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -1,15 +1,62 @@
 ﻿using System;
-
 namespace Test1
 {
-    class Program
+    public class Person
     {
-        static void Main(string[] args)
-        {
+        /// <summary>
+		/// set ID can't be null
+		/// </summary>
+		private int Id { get; set; }
 
-           
-        }
+		/// <summary>
+		/// set name can't be null
+		/// </summary>
+		private string Name { get; set; }
 
-       
-    }
+		/// <summary>
+		/// set age can't be null
+		/// </summary>
+		private int Age { get; set; }
+
+		/// <summary>
+		/// set gender can't be null
+		/// </summary>
+		private char Gender { get; set; }
+
+        /// <summary>
+		/// This function will 
+		/// </summary>
+		/// <returns>
+		/// Will return name of Person 
+		/// </returns>
+		public static string getName()
+		{
+			return Name;
+		}
+
+		/// <summary>
+		/// This function will get person age
+		/// </summary>
+		/// <returns>
+		/// Will return name of Person 
+		/// </returns>
+		public static string getAge()
+		{
+			return Age;
+		}
+
+	
+		/// <summary>
+		/// This function will grab all info on person 
+		/// </summary>
+		/// <returns>
+		/// Will return name of Person 
+		/// </returns>
+		public static Person getInfo(Person currPerson)
+		{
+			return currPerson;
+		}
+
+
+	}
 }
